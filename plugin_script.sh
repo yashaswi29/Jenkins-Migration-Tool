22df75cb2093302ff24e305bc2f1f48968194fc6
Yashaswi Tiwari <92193824+yashaswi29@users.noreply.github.com>
2024-08-05 19:59:00 +0530

    Update plugin_script.sh

--- plugin_script.sh
+++ plugin_script.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 set -x
-for plugin in $(cat plugins.txt); do
+while IFS= read -r plugin; do
     java -jar jenkins-cli.jar \
     -s http://3.111.55.36:8080/ \
     -auth @creds \
     install-plugin "$plugin"
-done
+done < plugins.txt
+
 java -jar jenkins-cli.jar \
     -s http://3.111.55.36:8080/ \
     -auth @creds \
     safe-restart
-
-
