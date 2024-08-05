4d7814ddc2fa53c06d73bf9f5cb442649d94afee
Yashaswi Tiwari <92193824+yashaswi29@users.noreply.github.com>
2024-08-05 19:41:11 +0530

    Update InstallPlugins.sh

--- InstallPlugins.sh
+++ InstallPlugins.sh
@@ -7,4 +7,4 @@ for plugin in $(cat plugins.txt); do \
     -auth yashaswi:yashaswi \
     install-plugin $plugin; \
 done
-java -jar /Users/yashaswitiwari/Developer/Groovy/py-jen/jenkins-cli.jar -s http://localhost:8080/ -auth yashaswi:yashaswi safe-restart
+java -jar /Users/yashaswitiwari/Developer/Groovy/py-jen/jenkins-cli.jar -s http://localhost:8080/ -auth @creds safe-restart
