5318325f8bcf60cf4f42555d464ba5b0ec6c220a
yashaswi29 <92193824+yashaswi29@users.noreply.github.com>
2024-08-07 11:19:04 +0530

    123

--- plugin_script.sh
+++ plugin_script.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-java -jar jenkins-cli.jar -s  http://3.7.254.62:8080/ -auth @creds who-am-i
+java -jar jenkins-cli.jar -s  http://3.109.5.227:8080/ -auth @creds who-am-i
 set -x
 for plugin in $(cat plugins.txt); do
     java -jar jenkins-cli.jar \
-    -s http://3.7.254.62:8080/ \
+    -s http://3.109.5.227:8080/ \
     -auth @creds \
     install-plugin "$plugin"
 done
 # java -jar jenkins-cli.jar \
-#     -s http://3.7.254.62:8080/ \
+#     -s http://3.109.5.227:8080/ \
 #     -auth @creds \
 #     safe-restart
 
