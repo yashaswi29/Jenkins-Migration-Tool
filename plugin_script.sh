d35d21d9ab55854133411bc136231f5420570275
yashaswi29 <92193824+yashaswi29@users.noreply.github.com>
2024-08-08 12:22:22 +0530

    Minute Changes in plugin script

--- plugin_script.sh
+++ plugin_script.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-
-java -jar jenkins-cli.jar -s  http://3.109.5.227:8080/ -auth @creds who-am-i
+java -jar jenkins-cli.jar -s  http://3.6.39.114:8080/ -auth @creds who-am-i
 set -x
 for plugin in $(cat plugins.txt); do
     java -jar jenkins-cli.jar \
-    -s http://3.109.5.227:8080/ \
+    -s http://3.6.39.114:8080/ \
     -auth @creds \
     install-plugin "$plugin"
 done
